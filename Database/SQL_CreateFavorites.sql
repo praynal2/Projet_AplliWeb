a6a94fd3c421bd5d68439d3fab5bb3afebda3498
Arkachu <108960010+Arkachu@users.noreply.github.com>
2023-05-12 11:12:46 +0200

    Delete SQL_CreateFavorites.sql

--- Database/SQL_CreateFavorites.sql
+++
@@ -1,8 +0,0 @@
-create table favorites (
-	favorite_id int not null auto_increment,
-    client_id int not null,
-    music_id int not null,
-    primary key (favorite_id),
-    foreign key (client_id) references clients (id),
-    foreign key (music_id) references musics (id)
-) ENGINE=InnoDB;
