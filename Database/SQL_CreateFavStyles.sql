80ebd5f822db17f729422036557f34228b42922c
Arkachu <108960010+Arkachu@users.noreply.github.com>
2023-05-12 11:12:30 +0200

    Delete SQL_CreateFavStyles.sql

--- Database/SQL_CreateFavStyles.sql
+++
@@ -1,9 +0,0 @@
-use Data;
-create table favoritestyles (
-	favstyle_id int not null auto_increment,
-    client_id int not null,
-    style_id int not null,
-    primary key (favstyle_id),
-    foreign key (client_id) references clients (id),
-    foreign key (style_id) references styles (id)
-) ENGINE=InnoDB;
