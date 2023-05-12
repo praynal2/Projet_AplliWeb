e3aeefe9d2a69fb724d0119757978a08cbad8ade
Arkachu <108960010+Arkachu@users.noreply.github.com>
2023-05-12 11:13:23 +0200

    Delete SQL_CreateTables.sql

--- Database/SQL_CreateTables.sql
+++
@@ -1,8 +0,0 @@
-create table favoritestyles (
-	favstyle_id int not null auto_increment,
-    client_id int not null,
-    style_id int not null,
-    primary key (favstyle_id),
-    foreign key (client_id) references clients (id),
-    foreign key (style_id) references styles (id)
-) ENGINE=InnoDB;
