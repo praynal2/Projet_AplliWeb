eed1042e9b7ecec186d0ffaf7552d37206ad6e6e
Lucas Dubois <lucas.dub92@gmail.com>
2023-05-24 14:29:30 +0200

    interface facade

--- Appliweb1/src/main/java/Pack/Facade.java
+++ Appliweb1/src/main/java/Pack/Facade.java
@@ -1,14 +1,13 @@
 package Pack;
 
 import javax.persistence.*;
+import java.util.List;
 
-public class Facade {
-
-	@PersistenceContext 
-	private EntityManager em;
-
-	
-	
-	
-	
+public interface Facade_Itf {
+	void addUser(String login, String password);
+	boolean isUser(String login, String password);
+	List<List<String>> getAllMusic(); // [id, name, genre, path]
+	List<Integer> getFavs(Integer idUser);
+	void addFavs(Integer idUser, List<Integer> idMusic);
+	void delFavs(Integer idUser, List<Integer> idMusic);
 }
