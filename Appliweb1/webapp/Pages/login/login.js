6e3f618e512d57bf3efd31b65e0a299ee588e976
arkachu <nic.roehlly@gmail.com>
2023-05-30 15:06:31 +0200

    Popup

--- Appliweb1/webapp/Pages/login/login.js
+++ Appliweb1/webapp/Pages/login/login.js
@@ -2,6 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
+const btnLogin = document.querySelector('.btnLogin');
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -14,4 +15,12 @@ loginLink.addEventListener('click', () => {
 btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
     wrapper.classList.remove('active');
+});
+
+btnLogin.addEventListener('click', () => {
+    if(isUser){
+        alert("Vous êtes connecté");
+    } else {
+        alert("Login ou mot de passe incorrect");
+    }
 });
